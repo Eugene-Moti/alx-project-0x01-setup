73296f96f1564cf0af6c3f9f0ff0403746363609
Eugene-Moti <erickmoti3609@gmail.com>
2025-06-01 19:33:24 +0300

    Update index-user file

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -51,5 +51,15 @@ const UsersPage = () => {
     </div>
   );
 };
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
 
 export default UsersPage;
